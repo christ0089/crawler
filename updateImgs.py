43de01cb10fef7089bc5d54f0b2a07e19c1a8668
Daniel <josephdzr@gmail.com>
2019-02-25 14:34:35 -0600

    Updated tags to include author's name in pictures

--- updateImgs.py
+++ updateImgs.py
@@ -8,13 +8,13 @@ import json
 import time
 import re
 # from sklearn.feature_extraction.text import TfidfVectorizer
-# from unsplash.api import Api
-# from unsplash.auth import Auth
+from unsplash.api import Api
+from unsplash.auth import Auth
 
 
-# client_id = "b88a0c3cdaba27de042293268caffaa460a689d5c7bc115a8b4bbd59f7f5f177"
-# client_secret = "b3c11f61346871a40d2924658828aa3995129df46cf6c1ddac8bdab5792acfb0"
-# redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
+client_id = "b88a0c3cdaba27de042293268caffaa460a689d5c7bc115a8b4bbd59f7f5f177"
+client_secret = "b3c11f61346871a40d2924658828aa3995129df46cf6c1ddac8bdab5792acfb0"
+redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
 
 
 def getImgUrls(API):
@@ -29,40 +29,49 @@ def getImgUrls(API):
             print(ID)
         else: 
             urls.write(ID)
-            urls.write(':')
-            urls.write(res[0]['url'])
+            urls.write('|')
+            urls.write(res[0]['medium'])
+            urls.write('|')
+            urls.write(res[0]['small'])
+            urls.write('|')
+            urls.write(res[0]['name'])
             urls.write('\n')
             # print(ID)
             # print(res[0]['url'])
     f.close()
     urls.close()
 
-def updateImgs():
-    urlFile= open('imgUrls.txt')
-    for line in urlFile:
+
+def updateTags():
+    pattern = re.compile('<div class=\\"entry-content\\">')
+    newContent= open('newContent.json', 'w+')
+    with open('data/newTags.json') as f:
+        tags= json.load(f)
+    urls= open('imgUrls.txt')
+    hashd= dict()
+    for line in urls:
         pts= line.split('|')
-        ID= pts[0]
-        url= pts[1]
+        hashd[pts[0]]= pts[1] + '|' + pts[2] + '|' + pts[3]
+        # print(pts[0])
+        # print(pts[1])
+        # print(pts[2])
+        # print(pts[3])
 
-        # db.reference('/AllArticles/'+ID).update({
-        #    'CoverImg':url 
-        # })
+    for line in tags:
+        pts= hashd[line].split('|')
+        print(pts[0])
+        print(pts[1])
+        print(pts[2])
 
+        tag= re.sub(pattern, '<div class=\"entry-content\"><img src="'+pts[0]+'">' + "<div>Photo from " + pts[2] + "</div>", tags[line])
+        hashd[line]= tag + '|' + pts[1]
+        print(hashd[line])
+
+    json.dump(hashd, newContent, ensure_ascii=False)
+    newContent.close()
+    f.close()
+
+updateTags()
 # auth = Auth(client_id, client_secret, redirect_uri)
 # api = Api(auth)
-# updateImgs()
-pattern = re.compile('<div class=\\"entry-content\\">')
-newContent= open('newContent.json', 'w+')
-with open('data/newTags.json') as f:
-    tags= json.load(f)
-urls= open('imgUrls.txt')
-hashd= dict()
-for line in urls:
-    pts= line.split('|')
-    hashd[pts[0]]= pts[1]
-for line in tags:
-    tag= re.sub(pattern, '<div class=\"entry-content\"><img src="'+hashd[line]+'">', tags[line])
-    hashd[line]= hashd[line] + '|' + tag
-
-json.dump(hashd, newContent, ensure_ascii=False)
 # getImgUrls(api)
